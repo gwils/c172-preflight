c86c9dd52d191e7af77d07bf819374c1fca802cf
Tony Morris <tmorris@tmorris.net>
2017-01-03 17:33:35 +1000

    weight

---
+++ src/Data/Aviation/Cessna172/Preflight/Weight.hs
@@ -0,0 +1,75 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE TemplateHaskell #-}
+
+module Data.Aviation.Cessna172.Preflight.Weight(
+  Weight
+, HasWeight(..)
+, HasWeights(..)
+, SetWeight(..)
+, HasWeight0(..)
+) where
+
+import Control.Category((.))
+import Control.Lens(Lens', Traversal', Setter', makeClassy, iso)
+import Data.Aviation.Units(Kilograms(kilograms), Pounds(pounds))
+import Data.Eq(Eq)
+import Data.Maybe(Maybe)
+import Data.Monoid(Monoid(mempty, mappend))
+import Data.Ord(Ord)
+import Data.Ratio((%))
+import Data.Semigroup(Semigroup((<>)))
+import Numeric.Lens(multiplying)
+import Prelude(Show, Rational, (+))
+
+newtype Weight =
+  Weight
+    Rational
+  deriving (Eq, Ord, Show)
+
+makeClassy ''Weight
+
+class HasWeights a where
+  weights ::
+    Traversal'
+      a
+      Weight
+
+instance HasWeights Weight where
+  weights =
+    weight
+
+class SetWeight a where
+  setWeight ::
+    Setter'
+      a
+      Weight
+
+instance SetWeight Weight where
+  setWeight =
+    weight
+
+class HasWeight0 a where
+  weight0 ::
+    Lens'
+      a
+      (Maybe Weight)
+
+instance Kilograms Weight where
+  kilograms =
+    multiplying (22046226218 % 10000000000) . pounds
+
+instance Pounds Weight where
+  pounds =
+    iso
+      Weight
+      (\(Weight x) -> x)
+
+instance Semigroup Weight where
+  (<>) =
+    mappend
+
+instance Monoid Weight where
+  mempty =
+    Weight 0
+  Weight w1 `mappend` Weight w2 =
+    Weight (w1 + w2)
