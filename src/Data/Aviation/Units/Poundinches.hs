94be82efbc41d96e6b8239853caa30f7731b007a
Tony Morris <tmorris@tmorris.net>
2017-01-03 22:09:44 +1000

    poundinches

--- src/Data/Aviation/Units/Poundinches.hs
+++ src/Data/Aviation/Units/Poundinches.hs
@@ -7,7 +7,7 @@ module Data.Aviation.Units.Poundinches(
 ) where
 
 import Control.Category(id)
-import Control.Lens(Getter, Iso', view, review)
+import Control.Lens(Getter, Iso', view, review, to)
 import Data.Aviation.Units.Pounds(Pounds(pounds))
 import Data.Aviation.Units.Inches(Inches(inches))
 import Prelude(Rational, (*))
@@ -19,3 +19,9 @@ class ToPoundinches a where
 instance ToPoundinches Rational where
   poundinches =
     id
+
+instance (Pounds p, Inches i) => ToPoundinches (p, i) where
+  poundinches =
+    to (\(p, i) -> let p' = review pounds p
+                       i' = review inches i
+                   in  p' * i')
