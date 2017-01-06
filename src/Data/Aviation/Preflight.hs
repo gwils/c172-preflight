132375ed6ecce285384beb44654939a0fb653304
Tony Morris <tmorris@tmorris.net>
2017-01-06 19:17:09 +1000

    weight and balance

--- src/Data/Aviation/Preflight.hs
+++ src/Data/Aviation/Preflight.hs
@@ -6,7 +6,7 @@ module Data.Aviation.Preflight where
 import Control.Category((.))
 import Control.Lens((^.))
 import Diagrams.Prelude(V2(V2), mkSizeSpec)
-import Data.Foldable(fold)
+import Data.Foldable(mapM_)
 import Data.Maybe(Maybe(Just))
 import Data.Monoid(mempty)
 import Data.Semigroup((<>))
@@ -14,41 +14,64 @@ import Data.Aviation.C172.Diagrams
 import Data.Aviation.C172.WB
 import Data.Aviation.Units
 import Data.Aviation.WB
+import Data.String(String)
 import System.IO(IO)
 
-tony = 80 ^. kilograms
-george = 85 ^. kilograms
-jess = 55 ^. kilograms
-
-dynamicWeights ::
-  C172Arms Weight
-dynamicWeights =
-  C172Arms
-    (tony <> george)
-    jess
-    -- being silly with fuel calculation, to demonstrate a point
-    -- the sum weight of
-    --   * 30 US gallons
-    --   * 15 litres
-    --   * 2 Imperial gallons
-    --   * 12 lb avgas
-    (fold [30 ^. usgallonsV . avgas100LL, 15 ^. litresV . avgas100LL, 2 ^. imperialgallonsV . avgas100LL, 12 ^. pounds])
-    (10 ^. kilograms)
-    mempty
+tonymorris = 80 ^. kilograms
+georgewilson = 85 ^. kilograms
+jessicaatherton = 55 ^. kilograms
+joshuamorris = 64 ^. kilograms
+amandaward = 100 ^. kilograms
+adammorris = 55 ^. kilograms
 
 vhlseBEW = 1691.6 ^. pounds
 vhlseArms = bewC172AircraftArms (40.6 ^. inches)
-testFlightMoment = totalC172Moment vhlseBEW dynamicWeights vhlseArms
+
+-- vhafrBEW = 1684.3 ^. pounds
+-- vhafrArms = 39.37 ^. inches
+
+flight20170102Weight ::
+  C172Arms Weight
+flight20170102Weight =
+  C172Arms
+    (tonymorris <> georgewilson)
+    jessicaatherton
+    (40 ^. usgallonsV . avgas100LL)
+    (10 ^. kilograms)
+    mempty
+
+flight20170121Weight ::
+  C172Arms Weight
+flight20170121Weight =
+  C172Arms
+    (tonymorris <> amandaward)
+    (adammorris <> joshuamorris)
+    (30 ^. usgallonsV . avgas100LL)
+    (10 ^. kilograms)
+    mempty
+
+flightMoments ::
+  [(String, Weight, C172Arms Weight, C172AircraftArms Arm, String)]
+flightMoments =
+  [
+    (
+      "20170102 Flight VH-LSE PAX: George, Jess"
+    , vhlseBEW
+    , flight20170102Weight
+    , vhlseArms
+    , "dist/flight20170102"
+    )
+  , (
+      "20170121 VH-LSE PAX: Amanda, Joshua, Adam"
+    , vhlseBEW
+    , flight20170121Weight
+    , vhlseArms
+    , "dist/flight20170121"
+    )
+  ]
 
 main ::
   IO ()
 main =
-  renderMomentDiagrams
-    "20170102 Test Flight VH-LSE PAX: George, Jess"
-    testFlightMoment
-    (mkSizeSpec (V2 (Just 3200) (Just 4524.8)))
-    "dist/output"
-
-
--- vhafrBEW = 1684.3 ^. pounds
--- vhafrArms = 39.37 ^. inches
+  mapM_ (\(s, w, ws, a, o) ->
+    renderMomentDiagrams s (totalC172Moment w ws a) (mkSizeSpec (V2 (Just 3200) (Just 4524.8))) o) flightMoments
