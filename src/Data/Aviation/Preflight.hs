7bdf8cbd6ee243f8d890dd4a8c5e2348212e4a7c
Tony Morris <tmorris@tmorris.net>
2017-01-25 10:56:51 +1000

    test flight for talk

--- src/Data/Aviation/Preflight.hs
+++ src/Data/Aviation/Preflight.hs
@@ -8,7 +8,6 @@ import Control.Lens((^.))
 import Diagrams.Prelude(V2(V2), mkSizeSpec)
 import Data.Foldable(mapM_)
 import Data.Maybe(Maybe(Just))
-import Data.Monoid(mempty)
 import Data.Semigroup((<>))
 import Data.Aviation.C172.Diagrams
 import Data.Aviation.C172.WB
@@ -18,95 +17,42 @@ import Data.String(String)
 import System.IO(IO)
 
 tonymorris = 80 ^. kilograms
-georgewilson = 85 ^. kilograms
-jessicaatherton = 55 ^. kilograms
-joshuamorris = 64 ^. kilograms
-amandaward = 100 ^. kilograms
-adammorris = 55 ^. kilograms
-gregdavis = 70 ^. kilograms
-robertdenney = 100 ^. kilograms
-jackmason = 70 ^. kilograms
+george = 85 ^. kilograms
+jessica = 55 ^. kilograms
+joshua = 64 ^. kilograms
 
+vhlseBEW :: Weight
 vhlseBEW = 1691.6 ^. pounds
+
+vhlseArms :: C172AircraftArms Arm
 vhlseArms = bewC172AircraftArms (40.6 ^. inches)
 
-vhafrBEW :: Weight
-vhafrBEW = 1684.3 ^. pounds
+vhvvoBEW :: Weight
+vhvvoBEW = 1684.3 ^. pounds
 
-vhafrArms :: C172AircraftArms Arm
-vhafrArms = bewC172AircraftArms (40.6 ^. inches)
+vhvvoArms :: C172AircraftArms Arm
+vhvvoArms = bewC172AircraftArms (40.6 ^. inches)
 
-flight20170102Weight ::
+flight20170131Weight ::
   C172Arms Weight
-flight20170102Weight =
+flight20170131Weight =
   C172Arms
-    (tonymorris <> georgewilson)
-    jessicaatherton
+    (tonymorris <> george)
+    (jessica <> joshua)
     (40 ^. usgallonsV . avgas100LL)
+    (30 ^. kilograms)
     (10 ^. kilograms)
-    mempty
-
-flight20170121Weight ::
-  C172Arms Weight
-flight20170121Weight =
-  C172Arms
-    (tonymorris <> amandaward)
-    (adammorris <> joshuamorris)
-    (30 ^. usgallonsV . avgas100LL)
-    (10 ^. kilograms)
-    mempty
-
-denneyDavisMasonVHLSE ::
-  C172Arms Weight
-denneyDavisMasonVHLSE =
-  C172Arms
-    (tonymorris <> robertdenney)
-    (gregdavis <> jackmason)
-    (24 ^. usgallonsV . avgas100LL)
-    (5 ^. kilograms)
-    mempty
-
-denneyDavisMasonVHAFR ::
-  C172Arms Weight
-denneyDavisMasonVHAFR =
-  C172Arms
-    (tonymorris <> robertdenney)
-    (gregdavis <> jackmason)
-    (26 ^. usgallonsV . avgas100LL)
-    (5 ^. kilograms)
-    mempty
 
 flightMoments ::
   [(String, Weight, C172Arms Weight, C172AircraftArms Arm, String)]
 flightMoments =
   [
     (
-      "20170102 Flight VH-LSE PAX: George, Jess"
-    , vhlseBEW
-    , flight20170102Weight
-    , vhlseArms
-    , "dist/flight20170102"
-    )
-  , (
-      "20170121 VH-LSE PAX: Amanda, Joshua, Adam"
-    , vhlseBEW
-    , flight20170121Weight
-    , vhlseArms
-    , "dist/flight20170121"
-    )
-  , (
-      "Hypothetical flight VH-LSE PAX: Robert Denney, Greg Davis, Jack Mason"
-    , vhlseBEW
-    , denneyDavisMasonVHLSE
-    , vhlseArms
-    , "dist/denneyDavisMasonVHLSE"
-    )
-  , (
-      "Hypothetical flight VH-AFR PAX: Robert Denney, Greg Davis, Jack Mason"
-    , vhafrBEW
-    , denneyDavisMasonVHAFR
-    , vhafrArms
-    , "dist/denneyDavisMasonVHAFR"
+      "20170131 Flight VH-VVO SIMULATION"
+    , vhvvoBEW
+    , flight20170131Weight
+    , vhvvoArms
+    , "dist/flight20170131"
     )
   ]
 
